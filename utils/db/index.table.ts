b31da169ea1a56e9888d09f2ff484ea35f726ca2
AnouarEssabiri <essabirianouar@gmail.com>
2025-09-19 02:28:24 +0100

    refactor: Consolidate object store creation into storeDefinitions array

--- utils/db/index.table.ts
+++ utils/db/index.table.ts
@@ -1,14 +1,25 @@
-import {openDB} from "idb";
+import { openDB } from "idb";
 
-export async function DB(){
-    return openDB("ezistraDB",1,{
-        upgrade(db){
-            if (!db.objectStoreNames.contains("users")){
-                db.createObjectStore("users", {keyPath: "id", autoIncrement: true})
-            }
-            if(!db.objectStoreNames.contains("documents")){
-                db.createObjectStore("documents", {keyPath:"id", autoIncrement:true})
-            }
+const storeDefinitions = [
+  { name: "users", options: { keyPath: "id", autoIncrement: true } },
+  { name: "documents", options: { keyPath: "id", autoIncrement: true } },
+  { name: "university", options: { keyPath: "id", autoIncrement: true } },
+  { name: "higher_education", options: { keyPath: "id", autoIncrement: true } },
+  { name: "complementary", options: { keyPath: "id", autoIncrement: true } },
+  { name: "address", options: { keyPath: "id", autoIncrement: true } },
+  { name: "baccalaureat", options: { keyPath: "id", autoIncrement: true } },
+  { name: "personal_info", options: { keyPath: "id", autoIncrement: true } },
+  { name: "university_info", options: { keyPath: "id", autoIncrement: true } },
+];
+
+export async function DB() {
+  return openDB("ezistraDB", 1, {
+    upgrade(db) {
+      storeDefinitions.forEach(store => {
+        if (!db.objectStoreNames.contains(store.name)) {
+          db.createObjectStore(store.name, store.options);
         }
-    })
+      });
+    }
+  });
 }
